5555bf0cf7f7a524323bd8b1c1e442007c03b7a4
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2018-12-19 02:07:55 +0900

    [add] カウンターを実装

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,10 +1,26 @@
 <template>
+<div>
+    <div>{{ count }}回クリック</div>
+    <!-- Vueの中でクリックしたらincrementメソッドを呼び出す-->
+    <button v-on:click="increment">カウントを増やす</button>
     <div class='counter'>Count</div>
+</div>
 </template>
 
 <script>
 export default {
     name: "Count",
+    data() {
+        return {
+            //一番最初に初期化する
+            count: 0
+        };
+    },
+    methods: {
+        increment: function() {
+            this.count += 1;
+        }
+    }
 };
 </script>
 
